7811f19a622f082a316c0175f6ce00564332275f
Maschenko Sofia <m.sofia05@mail.ru>
2024-02-07 20:36:37 +0300

    finish ex3

--- module 14/ex 3/script.js
+++ module 14/ex 3/script.js
@@ -1,76 +1,35 @@
+let btn = document.querySelector('.button');
 
+btn.addEventListener('click', () => {
+    const limitValue = parseInt(document.querySelector('.input').value);
 
-// // `https://jsonplaceholder.typicode.com/photos?_limit=${value}`
+    if (isNaN(limitValue) || limitValue < 1 || limitValue > 10) {
+        document.querySelector('.output').innerHTML = 'Введите корректное число в диапазоне от 1 до 10';
+    } else {
+        const xhr = new XMLHttpRequest();
+      
+        xhr.open('GET', 'https://jsonplaceholder.typicode.com/photos?_limit=' + limitValue);
+        xhr.onload = function() {
+            const response = JSON.parse(xhr.response);
+            const outputContainer = document.querySelector('.output');
+            
+            response.forEach(photo => {
+                const cards = `
+                <div class="card">
+                    <img src="${photo.thumbnailUrl}" class="card-image"/>
+                </div>
+                `;
 
+                outputContainer.innerHTML += cards;            
+            });
+            
+        };
 
-// 1
+        xhr.onerror = function() {
+            document.querySelector('.output').innerHTML = 'Ошибка запроса! Пожалуйста, повторите позже.';
+            console.log('Ошибка запроса! Статус ответа: ', xhr.status);
+        };
 
-// function makeRequest(url) {
-//     const xhr = new XMLHttpRequest();
-//     xhr.open("GET", url);
-//     xhr.onload = function() {
-//         if (xhr.status != 200) {
-//             console.log('Статус ответа: ', xhr.status);
-//           } else {
-//             console.log('Результат: ', JSON.parse(xhr.response));
-//           }
-//     };
-//     xhr.onprogress = function(event) {
-//         console.log(`Загружено ${event.loaded} из ${event.total}`)
-//     };
-//     xhr.onerror = function() {
-//         console.log('Ошибка запроса! Статус ответа: ', xhr.status);
-//     };
-//     xhr.send();
-// };
-// 
-// let btn = document.querySelector('.button');
-// btn.addEventListener('click', () => {
-//     if (1 <= limitValue <= 10) {
-//         makeRequest(`https://jsonplaceholder.typicode.com/photos?_limit=${limitValue}`);
-//     } else {
-//         alert('Число вне диапазона от 1 до 10')
-//     }
-// });
-
-
-// 2
-
-
-// function handleClick() {   
-//         const limitValue = document.querySelector('.input').value;
-//         // let limitValue = document.getElementById('number').value;      
-//         if(limitValue < 1 || limitValue > 10) {     
-//             document.querySelector('.output').innerHTML = 'число вне диапазона от 1 до 10';   
-//         } else {       
-//             const xhr = new XMLHttpRequest();     
-//             xhr.open('GET', 'https://jsonplaceholder.typicode.com/photos?_limit=' + limitValue);     
-//             xhr.onload = function() {       
-//                 document.querySelector('.output').innerHTML = xhr.responseText;    
-//             }     
-//             xhr.onprogress = function(event) {
-//                 console.log(`Загружено ${event.loaded} из ${event.total}`)
-//             };
-//             xhr.onerror = function() {
-//                 console.log('Ошибка запроса! Статус ответа: ', xhr.status);
-//             };
-//             xhr.send();   
-//         } 
-// } 
-
-
-// 3
-
-// function handleClick() {   
-//     let number = document.getElementById('number').value;      
-//     if(number < 1 || number > 10) {    
-//          document.getElementById('output').innerHTML = 'число вне диапазона от 1 до 10';   
-//     } else {       
-//         let xhr = new XMLHttpRequest();     
-//         xhr.open('GET', 'https://jsonplaceholder.typicode.com/photos?_limit=' + number);     
-//         xhr.onload = function() {       
-//             document.getElementById('output').innerHTML = xhr.responseText;     
-//         }     
-//         xhr.send();  
-//     } 
-// } 
+        xhr.send();
+    }
+});
