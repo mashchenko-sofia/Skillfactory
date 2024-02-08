314536d120c8eb096947663a9ed2597997f09809
Maschenko Sofia <m.sofia05@mail.ru>
2024-02-08 18:22:23 +0300

    add using localStorage

--- module 14/ex 5/script.js
+++ module 14/ex 5/script.js
@@ -1,11 +1,17 @@
 const makeRequest = (value1, value2) => {
     return fetch(`https://jsonplaceholder.typicode.com/photos?_page=${value1}&_limit=${value2}`)
     .then(response => response.json())
-    .then(data => data)
+    .then(data => {
+        return localStorage.setItem('lastRequest', JSON.stringify(data));
+    })
     .catch(() => {
         console.log('error');
-        return document.querySelector('.output').innerHTML = 'Error';
+        return renderOutput('Error');
     })
+}
+
+const renderOutput = (output) => {
+    document.querySelector('.output').innerHTML += output;
 }
 
 const btn = document.querySelector('.button');
@@ -14,14 +20,28 @@ btn.addEventListener('click', async () => {
     const value1 = parseInt(document.querySelector('.input-1').value);
     const value2 = parseInt(document.querySelector('.input-2').value);
 
-    if ((1 <= value1 <= 10) && (1 <= value2 <= 10)) {
-        const requestResult = await makeRequest(value1, value2);
-        return document.querySelector('.output').innerHTML += `<img src="${requestResult.thumbnailUrl}">`;  
-    } else if (isNaN(value1) || !(1 <= value1 <= 10)) {
-        return document.querySelector('.output').innerHTML = 'Номер страницы вне диапазона от 1 до 10';
-    } else if (isNaN(value2) || !(1 <= value2 <= 10)) {
-        return document.querySelector('.output').innerHTML = 'Лимит вне диапазона от 1 до 10';
-    } else {
-        return document.querySelector('.output').innerHTML = 'Номер страницы и лимит вне диапазона от 1 до 10';
+    if ((value1 <= 1 || value1 >= 10 || isNaN(value1)) && (value2 <= 1 || value2 >= 10 || isNaN(value2))) {
+        return renderOutput('Номер страницы и лимит вне диапазона от 1 до 10');
+    } 
+    if (isNaN(value1) || (value1 <= 1 || value1 >= 10)) {
+        return renderOutput('Номер страницы вне диапазона от 1 до 10');
+    } 
+    if (isNaN(value2) || (value2 <= 1 || value2 >= 10)) {
+        return renderOutput('Лимит вне диапазона от 1 до 10');
     }
+        
+    const requestResult = await makeRequest(value1, value2);
+    const lastRequest = JSON.parse(localStorage.getItem('lastRequest'))
+    
+    lastRequest.forEach(el => {
+        const imgInStorage = `<img src="${el.thumbnailUrl}">`;
+
+        renderOutput(imgInStorage)
+    })
+
+    requestResult.forEach(el => {
+        const img = `<img src="${el.thumbnailUrl}">`;
+
+        renderOutput(img);  
+    });
 });
