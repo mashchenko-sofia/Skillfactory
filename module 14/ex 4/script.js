3a369271be63cbe5929bb19b868fa00f7d3e84f1
Maschenko Sofia <m.sofia05@mail.ru>
2024-02-08 13:46:49 +0300

    add script link

--- module 14/ex 4/script.js
+++ module 14/ex 4/script.js
@@ -1,15 +1,7 @@
-const value1 = parseInt(document.querySelector('.input-1').value);
-const value2 = parseInt(document.querySelector('.input-2').value);
-
-const makeRequest = () => {
+const makeRequest = (value1, value2) => {
     return fetch(`https://dummyimage.com/${value1}x${value2}/`)
     .then((response) => { 
-        console.log('response', response);
-        return response.json;
-    })
-    .then((json) => { 
-        console.log(json); 
-        return json;
+        return response;
     })
     .catch(() => { 
         document.querySelector('.output').innerHTML = 'Error';
@@ -20,14 +12,13 @@ const makeRequest = () => {
 let btn = document.querySelector('.button');
 
 btn.addEventListener('click', async () => {
+    const value1 = parseInt(document.querySelector('.input-1').value);
+    const value2 = parseInt(document.querySelector('.input-2').value);
     if (!(100 <= value1 <= 300) || !(100 <= value2 <= 300)) {
         document.querySelector('.output').innerHTML = 'Одно из чисел вне диапазона от 100 до 300';
     } else {
-        const requestResult = await makeRequest();
+        const requestResult = await makeRequest(value1, value2);
 
-        const img = await requestResult.blob(); // браузер не видит blob и выдает 'blob is not a function'
-        const imgURL = URL.createObjectURL(img); 
-
-        document.querySelector('.output').innerHTML += `<img src="${imgURL}">`;  
+        document.querySelector('.output').innerHTML += `<img src="${requestResult.url}">`;  
     }
 });
