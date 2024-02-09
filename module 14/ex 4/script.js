4f105ef4f6250557a4e13a15da47825e1d74a014
Maschenko Sofia <m.sofia05@mail.ru>
2024-02-09 11:58:13 +0300

    add validateInput function

--- module 14/ex 4/script.js
+++ module 14/ex 4/script.js
@@ -1,3 +1,27 @@
+const MIN_IMAGE_SIZE = 100;
+const MAX_IMAGE_SIZE = 300;
+
+
+let btn = document.querySelector('.button');
+
+btn.addEventListener('click', async () => {
+    const widthImageValue = parseInt(document.querySelector('.input-1').value);
+    const heightImageValue = parseInt(document.querySelector('.input-2').value);
+
+    if (!validateInput(widthImageValue, MIN_IMAGE_SIZE, MAX_IMAGE_SIZE) || 
+    !validateInput(heightImageValue, MIN_IMAGE_SIZE, MAX_IMAGE_SIZE)) {
+        document.querySelector('.output').innerHTML = 'Одно из чисел вне диапазона от 100 до 300';
+    } else {
+        const requestResult = await makeRequest(widthImageValue, heightImageValue);
+
+        document.querySelector('.output').innerHTML += `<img src="${requestResult.url}">`;  
+    }
+});
+
+const validateInput = (value, minValue, maxValue) => {
+    return value >= minValue && value <= maxValue;
+}
+
 const makeRequest = (value1, value2) => {
     return fetch(`https://dummyimage.com/${value1}x${value2}/`)
     .then((response) => { 
@@ -8,17 +32,3 @@ const makeRequest = (value1, value2) => {
         console.log('error');
     });
 }
-
-let btn = document.querySelector('.button');
-
-btn.addEventListener('click', async () => {
-    const value1 = parseInt(document.querySelector('.input-1').value);
-    const value2 = parseInt(document.querySelector('.input-2').value);
-    if (!(100 <= value1 <= 300) || !(100 <= value2 <= 300)) {
-        document.querySelector('.output').innerHTML = 'Одно из чисел вне диапазона от 100 до 300';
-    } else {
-        const requestResult = await makeRequest(value1, value2);
-
-        document.querySelector('.output').innerHTML += `<img src="${requestResult.url}">`;  
-    }
-});
